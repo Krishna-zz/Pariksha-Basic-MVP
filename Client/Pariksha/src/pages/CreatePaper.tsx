2fc38d0fc7a974be4482f7b6799c2fc4c067b8cb
Krishna-zz <krishnagunjal022@gmail.com>
2026-04-28 14:07:14 +0530

    Paper created

--- Client/Pariksha/src/pages/CreatePaper.tsx
+++ Client/Pariksha/src/pages/CreatePaper.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import API from '../api/api'
 
 const CreatePaper = () => {
 
+  const [title , setTitle] = useState("");     //this is to write paper's title
+
+  const handleSubmit = async() => {
+      await API.post("/papers",{
+        title, 
+        questions:[]
+      })
+      alert("Paper created")
+  }
+
   return (
     <div>
-        <h1 className='bg-amber-400'>This is my biggest project ever</h1>
+       <input type="text" 
+       placeholder='Title'
+       value={title}
+       onChange={(e) => setTitle(e.target.value)}/>
+       <button>Create</button>
     </div>
   )
 }
