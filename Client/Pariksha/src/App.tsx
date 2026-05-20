e4e6053a490b16f41bfb7dc6d10724e472b67902
Krishna-zz <krishnagunjal022@gmail.com>
2026-05-20 15:42:31 +0530

    added routes

--- Client/Pariksha/src/App.tsx
+++ Client/Pariksha/src/App.tsx
@@ -1,5 +1,8 @@
 import CreatePaper from "./pages/CreatePaper"
 import AllPapers from "./pages/AllPapers"
+import ExamAttempt from "./pages/AttemptExam/Examattempt";
+import ExamEntry from "./pages/AttemptExam/Examentry";
+import ExamResult from "./pages/AttemptExam/Examresult";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -10,6 +13,11 @@ function App() {
      <Routes>
         <Route path="/create" element={<CreatePaper/>} />
         <Route path="/viewpapers" element={<AllPapers/>} />
+
+
+        <Route path="/exam/:examId"         element={<ExamEntry />} />
+        <Route path="/exam/:examId/attempt" element={<ExamAttempt />} />
+        <Route path="/exam/:examId/result"  element={<ExamResult />} />
       </Routes>
      
     </>
