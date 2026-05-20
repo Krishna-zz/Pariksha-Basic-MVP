80fbe862a64c87c304bfbff022afb9b85aac7b2f
Krishna-zz <krishnagunjal022@gmail.com>
2026-05-20 16:17:31 +0530

    ECP frontend routes

--- Client/Pariksha/src/App.tsx
+++ Client/Pariksha/src/App.tsx
@@ -3,6 +3,9 @@ import AllPapers from "./pages/AllPapers"
 import ExamAttempt from "./pages/AttemptExam/Examattempt";
 import ExamEntry from "./pages/AttemptExam/Examentry";
 import ExamResult from "./pages/AttemptExam/Examresult";
+import CreateExam from "./pages/ECP/CreateExam";
+import ExamDashboard from "./pages/ECP/ExamDashboard";
+import ExamMonitor from "./pages/ECP/ExamMonitor";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -11,10 +14,16 @@ function App() {
   return (
     <>
      <Routes>
+         {/* QPB */}
         <Route path="/create" element={<CreatePaper/>} />
         <Route path="/viewpapers" element={<AllPapers/>} />
 
+        {/* ECP */}
+        <Route path="/ecp/dashboard" element={<ExamDashboard />} />
+        <Route path="/ecp/create" element={<CreateExam />} />
+        <Route path="/ecp/monitor/:examId" element={<ExamMonitor />} />
 
+        {/* SEE */}
         <Route path="/exam/:examId"         element={<ExamEntry />} />
         <Route path="/exam/:examId/attempt" element={<ExamAttempt />} />
         <Route path="/exam/:examId/result"  element={<ExamResult />} />
